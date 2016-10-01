39c6e026af1bb8de72de1dd04cca28c714db5321
Spencer Fox <spencer.fox85@gmail.com>
2016-10-01 15:55:39 -0500

    initial commit

---
+++ arundo_code/arundo_data_read.R
@@ -0,0 +1,22 @@
+
+library(data.table)
+arundo <- fread("~/projects/arundo/data/CLASS_sensor_data.csv", header=T, skip=1, drop=c(1), data.table=FALSE)
+
+arundo <- t(arundo)
+
+arundo <- as.data.frame(arundo, row.names = NULL)
+arundo$time <- rownames(arundo)
+rownames(arundo) <- NULL
+head(arundo)
+colnames(arundo) <- arundo[1,]
+colnames(arundo)[length(colnames(arundo))] <- "time"
+
+arundo <- arundo[-1, ]
+head(arundo)
+write_csv(arundo, "~/projects/arundo/data/sensor_data_transposed.csv")
+
+library(tidyverse)
+sensor_only <- arundo %>% select("59":"63", time)
+write_csv(sensor_only, "~/projects/arundo/data/p1_sensor_data.csv")
+
+temp <- read_csv("~/projects/arundo/data/p1_sensor_data.csv")
---
+++ arundo_code/init_analysis.R
@@ -0,0 +1,39 @@
+
+
+library(tidyverse)
+library(lubridate)
+library(cowplot)
+library(magrittr)
+
+
+arundo <- read_csv("data/p1_sensor_data.csv")
+arundo <- arundo[-nrow(arundo), ]
+colnames(arundo) <- ifelse(colnames(arundo)!="time", paste("sensor_", colnames(arundo), sep=""), colnames(arundo))
+
+arundo$time <- mdy_hms(arundo$time)
+
+################################
+## Plot the time series
+################################
+
+ts_plot <- arundo %>% gather(sensor, reading, 1:5) %>%
+  ggplot(aes(time, reading, color=sensor)) + geom_line() %>%
+
+save_plot(filename = "figs/sensor_ts.pdf", plot = ts_plot, base_height = 5, base_aspect_ratio = 2)
+
+################################
+## Run linear model
+################################
+library(caret)
+
+
+## First test running single train test phase
+train_ctrl <- trainControl(method = "cv", number = 10, savePredictions="all", p = 0.01, returnResamp="all")
+
+cv_fit <- train(sensor_62~ sensor_59+sensor_60 +sensor_61 + sensor_63, 
+                    data = arundo, 
+                    method = "lm",
+                    trControl = train_ctrl)
+
+print(cv_fit[["resample"]])
+?train
